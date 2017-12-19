fc2f8bd0001b4fb701aa1e587cb07c8a9595ee1e
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-19 19:51:45 +0100

    Mise en place de la conexion Bdd

--- index.php
+++ index.php
@@ -9,4 +9,13 @@
     <body>
         <h1>Bienvenue sur tf1 !</h1>
         <p>Les News de tf1 :</p>
- 
+ <?php
+//Mise ne place pour la Connexion à la base de données 
+try
+    {
+         $bdd = new PDO('mysql:host=localhost;dbname=tf1;charset=utf8', 'root', '');
+    }
+catch(Exception $e)
+    {
+        die('Erreur : '.$e->getMessage());
+    }
