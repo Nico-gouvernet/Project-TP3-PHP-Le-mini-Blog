cabfd20f3a38d916f16c46d93273103f64b0d3fd
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-19 20:00:31 +0100

    Mise en place du formulaire pour la conexion

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
         <title>Mon blog</title>
     <link href="style.css" rel="stylesheet" /> 
     </head>
-        
-    <body>
-        <h1>Bienvenue sur tf1 !</h1>
+<body>
+     <h1>Bienvenue sur tf1 !</h1>
         <p>Les News de tf1 :</p>
- <?php
+<?php
 //Mise ne place pour la Connexion à la base de données 
 try
     {
@@ -19,30 +18,43 @@ catch(Exception $e)
     {
         die('Erreur : '.$e->getMessage());
     }
+
 // On récupère les 5 derniers billets
 $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%m/%Y à %Hh%imin%ss\') AS date_creation_fr FROM tf1 ORDER BY date_creation DESC LIMIT 0, 5');
 
 while ($donnees = $req->fetch())
     {
 ?>
-<div class="news">
+    <div class="news">
          <h3>
             <?php echo htmlspecialchars($donnees['titre']); ?>
             <em>le <?php echo $donnees['date_creation_fr']; ?></em>
         </h3>
     
     <p>
-<?php
+        <?php
             // On affiche le contenu du billet
         echo nl2br(htmlspecialchars($donnees['contenu']));
-?>
+        ?>
    
     <br />
         <em><a href="comentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
     </p>
 </div>
+
 <?php
     } // Fin de la boucle des billets
 
 $req->closeCursor();
 ?>
+<p> Veuillez entrer le mot de passe pour accéder au fichier <strong>secret.php</strong></p>
+<form action="password.php" method="post">
+            <p>
+            <input type="password" name="password" />
+            <input type="submit" value="Valider" />
+            </p>
+        </form>
+        <p> Si vous ne travaillez pas chez TF1 ;) , inutile d'insister vous ne trouverez jamais le mot de passe ! pour accéder au archives du JT;-)</p>
+         <img src="images/logo.png" alt="Photo de TF1" title="TF1"/>
+</body>
+</html>
