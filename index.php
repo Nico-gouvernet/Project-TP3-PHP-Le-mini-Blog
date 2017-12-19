53e23327aa96d11be15381f1f491e96a238954d7
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-19 19:19:17 +0100

    Structure de base du fichier

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mon blog</title>
+    <link href="style.css" rel="stylesheet" /> 
+    </head>
+        
+    <body>
+        <h1>Bienvenue sur tf1 !</h1>
+        <p>Les News de tf1 :</p>
+ 
