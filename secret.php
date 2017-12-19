506cdf51d710152de0705d76822e504bfbff9a82
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-19 20:20:13 +0100

    Fichier secret.php

---
+++ secret.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Mini-chat</title>
+    </head>
+    <style>
+        form {
+            background-color:#895959;
+            color: white;  
+            text-align: center; 
+            font-family:sans-serif;
+            font-size: 1.2em;
+            }
+             body {
+             background-color: #B78178;
+            }
+    </style>
+    <body>
+<form action="minichat_post.php" method="post">
+        <p>
+        <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+        <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
+
+        <input type="submit" value="Envoyer" />
+    </p>
+    </form>
+    <?php
+// Connexion à la base de données
+try
+{
+    $bdd = new PDO('mysql:host=localhost;dbname=test;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
+// Récupération des 10 derniers messages
+$reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+
+// Affichage de chaque message (toutes les données sont protégées par htmlspecialchars)
+while ($donnees = $reponse->fetch())
+{
+    echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+}
+
+$reponse->closeCursor();
+
+?>
+</body>
+</html>
