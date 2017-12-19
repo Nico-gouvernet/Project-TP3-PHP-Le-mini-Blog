5584d7366eac536a59c60ab8b140d77bc8d78489
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-19 20:08:23 +0100

    Conexion de la Bdd

--- comentaires.php
+++ comentaires.php
@@ -9,3 +9,13 @@
     <body>
         <h1>Mon super blog !</h1>
         <p><a href="index.php">Retour à la liste des billets</a></p>
+<?php
+// Connexion à la base de données
+try
+{
+	$bdd = new PDO('mysql:host=localhost;dbname=tf1;charset=utf8', 'root', '');
+}
+catch(Exception $e)
+{
+        die('Erreur : '.$e->getMessage());
+}
