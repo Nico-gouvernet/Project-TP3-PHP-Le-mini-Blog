79c0d9262efe0295d3993990c03f7de7c668b4e1
nico.gouvernet <nicolas-gouvernet@outlook.fr>
2017-12-19 19:57:31 +0100

    Mise en place de la class News

--- index.php
+++ index.php
@@ -25,3 +25,19 @@ $req = $bdd->query('SELECT id, titre, contenu, DATE_FORMAT(date_creation, \'%d/%
 while ($donnees = $req->fetch())
     {
 ?>
+<div class="news">
+         <h3>
+            <?php echo htmlspecialchars($donnees['titre']); ?>
+            <em>le <?php echo $donnees['date_creation_fr']; ?></em>
+        </h3>
+    
+    <p>
+<?php
+            // On affiche le contenu du billet
+        echo nl2br(htmlspecialchars($donnees['contenu']));
+?>
+   
+    <br />
+        <em><a href="comentaires.php?billet=<?php echo $donnees['id']; ?>">Commentaires</a></em>
+    </p>
+</div>
